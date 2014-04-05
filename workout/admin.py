bdb3c83d3ef8b80041e032c096d4d571004c95ad
Electric Erger <electricerger@gmail.com>
2014-04-05 07:28:29 -0400

    Minor edits to the admin page. Timezones are handled by Django itself

--- workout/admin.py
+++ workout/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from workout.models import ExerciseEvent
 
-admin.site.register(ExerciseEvent)
-# Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,				{'fields': ['woType']}),
+        ('Clock', 			{'fields': ['startTime', 'endTime']}),
+        ('Timing', 			{'fields': ['elapsedTime']})
+
+    ]
+    list_display = ('id', 'woType' ,'startTime')
+
+
+admin.site.register(ExerciseEvent, EventAdmin)
